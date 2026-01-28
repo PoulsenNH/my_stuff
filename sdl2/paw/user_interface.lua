8e8ba207d29c30da5c54483d16747e414c98264d
HNesluoptean <heathpoulsen@aol.com>
2026-01-27 20:51:33 -0700

    Add files via upload

---
+++ sdl2/paw/user_interface.lua
@@ -0,0 +1,10 @@
+-- This file is used for controlling the entirety of the user interface inside of the main.c file, this file is not to be used for any other files
+
+user_interface = {
+    version = "0.1.0-alpha",
+
+    side_menu = {
+        button_amount = 6,
+        button_names = {"Home", "Artificial Intelligence", "Notes", "Check List", "Settings", "About"},
+    }
+}
